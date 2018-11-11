c1ba6b2f5b7e89cb98c333bfeeb62ec22f585d24
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-11-11 13:57:28 -0600

    12 - Soporte de EcmaScript

---
+++ babel-loader/index.js
@@ -0,0 +1,7 @@
+import './styles.css'
+
+import { firstMessage } from "./message.js"
+
+document.write(`<h1>${firstMessage}</h1>`)
+
+console.log("Hello World, from webpack in a webpack config file");
