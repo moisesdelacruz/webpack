289ea67ddc911f1ec2a3b66f7bb94952a64e5354
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-11-11 15:26:54 -0600

    14 - Soporte de Imágenes en Webpack

---
+++ url-loader-images/index.js
@@ -0,0 +1,17 @@
+import './styles.css'
+
+import platziImg from './platzi.png'
+
+import { firstMessage, delayedMessage } from "./message"
+
+document.write(`<h1>${firstMessage}</h1>`)
+delayedMessage()
+
+const img = document.createElement('img')
+img.setAttribute('src', platziImg)
+img.setAttribute('width', '50px')
+img.setAttribute('height', '50px')
+
+document.body.append(img)
+
+console.log("Hello World, from webpack in a webpack config file");
