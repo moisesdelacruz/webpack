e8c57c54297d09b8ceb075364eb993e327efe28b
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-11-10 20:04:32 -0600

    7 - Cargando configuración de rutas relativas

--- external/webpack.config.js
+++ external/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: path.resolve(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'bundle.js'
   }
 };
