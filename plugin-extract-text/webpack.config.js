0fe85a47866798fa521daeb9996c188026016b43
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2018-11-10 21:11:15 -0600

    9 - Plugins

---
+++ plugin-extract-text/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // loaders
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
+};
